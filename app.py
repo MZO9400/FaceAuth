eaf23fc7381fa13926884ccd2f258ab29f71b45b
MZO9400 <hamza.hameed00@yahoo.com>
2021-06-12 04:32:45 +0500

    Create basic API and template

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, jsonify
+from PIL import Image
+from api.face import FaceVerification
+from db.mongo import FaceEncodings
+from decouple import config
+import cv2
+import numpy
+
+app = Flask(__name__)
+
+face_verification = FaceVerification(
+    FaceEncodings(
+        config("DATABASE_URI")
+    )
+)
+
+@app.route("/")
+def hello():
+    return render_template('authenticate.html')
+
+@app.route('/register', methods=["POST"])
+def register():
+    img = Image.open(request.files['file'])
+    img = np.array(img)
+    img = cv2.cvtColor(np.array(img), cv2.COLOR_BGR2RGB)
+    face_verification.registration(img, request.form['username'])
