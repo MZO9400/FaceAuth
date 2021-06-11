865468ed032610996ddce01f99febf0e6a12a7b8
MZO9400 <hamza.hameed00@yahoo.com>
2021-06-12 02:51:22 +0500

    Create APIs

---
+++ api/face.py
@@ -0,0 +1,13 @@
+import face_recognition
+
+def registration():
+    pass
+
+def authenticate():
+    pass
+
+def get_facial_structure():
+    pass
+
+def count_people_in_image():
+    pass
---
+++ db/mongo.py
@@ -0,0 +1,5 @@
+def insert():
+    pass
+
+def fetch():
+    pass
