246c9af4c5248f9e1fec08a82a945bdc47aece7c
MZO9400 <hamza.hameed00@yahoo.com>
2021-06-12 03:30:23 +0500

    FaceEncodings database class!

--- db/mongo.py
+++ db/mongo.py
@@ -1,5 +1,21 @@
-def insert():
-    pass
+import pymongo
 
-def fetch():
-    pass
+class FaceEncodings:
+    def __init__(self, connection_string):
+        self.__client__ = pymongo.MongoClient(connection_string)
+        self.collection = self.__client__['face_data']['encodings']
+
+    def insert(self, user):
+        print(user)
+        if (user['username'] is not None) and (user['encodings'] is not None):
+            user['encodings'] = user['encodings'].tolist()
+            self.collection.insert_one(user)
+            return True
+        else:
+            return False
+
+    def fetch(self, username = None):
+        if username:
+            return self.collection.find_one({'username': username})
+        else:
+            return self.collection.find()
