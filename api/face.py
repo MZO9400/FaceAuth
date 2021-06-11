97646d4349a6871c8cb2d114d7b5634910b40e7f
MZO9400 <hamza.hameed00@yahoo.com>
2021-06-12 04:03:12 +0500

    Authentication API: Compare face from image with database to authenticate

--- api/face.py
+++ api/face.py
@@ -8,7 +8,37 @@ class FaceVerification:
         pass
 
     def authenticate(self, image, username = None):
-        pass
+        if self.count_people_in_image(image) != 1:
+            return {
+                "success": False,
+                "error": "Picture should only have one person"
+            }
+        unknown_encodings = self.get_facial_structure(image)
+        if username:
+            person = self.client.fetch(username)
+            if not person:
+                return {
+                    "success": False,
+                    "error": "Username not found"
+                }
+            comparison_results = face_recognition.compare_faces([person['encodings']], unknown_encodings)
+            return {
+                "success": comparison_results[0]
+            }
+        else:
+            people = list(self.client.fetch())
+            people_encodings = list(map(lambda row: row['encodings'], people))
+            face_array = face_recognition.compare_faces(people_encodings, unknown_encodings)
+            index = face_array.index(True)
+            if index is not None:
+                return {
+                    "success": True,
+                    "username": people[index]['username']
+                }
+            else:
+                return {
+                    "success": False
+                }
 
     def get_facial_structure(self, image):
         return face_recognition.face_encodings(image)[0]
